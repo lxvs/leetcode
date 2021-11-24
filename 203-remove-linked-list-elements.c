c7bfa9e68f5b8cec1283b96c495be0a672bdfcbd
lxvs <jn.apsd@gmail.com>
2021-11-24 18:35:28 +0800

    203: Remove Linked List Elements

---
+++ 203-remove-linked-list-elements.c
@@ -0,0 +1,25 @@
+#include "lib/llist.h"
+
+#include <stdlib.h>
+
+struct ListNode* removeElements(struct ListNode* head, int val){
+    struct ListNode * p;
+
+    if (!head)
+        return NULL;
+
+    while (head && head->val == val) {
+        p = head;
+        head = head->next;
+        free(p);
+    }
+
+    for (struct ListNode * q = head; q && q->next; q = q->next)
+        while (q->next && q->next->val == val) {
+            p = q->next;
+            q->next = q->next->next;
+            free(p);
+        }
+
+    return head;
+}
