1c1272727cf66039870bbea30c967fdfdae06d8c
lxvs <jn.apsd@gmail.com>
2021-11-24 09:59:02 +0800

    074: Search a 2D Matrix - Binary search

--- 074-search-a-2d-matrix.c
+++ 074-search-a-2d-matrix.c
@@ -2,13 +2,30 @@
 
 bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target){
     int i = 0;
-    int j = 0;
+    int l = 0;
+    int r, m;
+
+    if (target < matrix[0][0] ||
+            target > matrix[matrixSize - 1][matrixColSize[matrixSize - 1] - 1])
+        return false;
 
     while (i < matrixSize - 1 && *matrix[i + 1] <= target)
         i++;
 
-    while (j < matrixColSize[i] && matrix[i][j] < target)
-        j++;
+    r = matrixColSize[i] - 1;
 
-    return j < matrixColSize[i] ? matrix[i][j] == target : false;
+    while (l <= r) {
+        m = (l + r) / 2;
+        if (target < matrix[i][m]) {
+            r = m - 1;
+            continue;
+        }
+        if (target > matrix[i][m]) {
+            l = m + 1;
+            continue;
+        }
+        return true;
+    }
+
+    return false;
 }
