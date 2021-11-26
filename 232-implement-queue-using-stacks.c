092c4d7b4bf07da2206b64de6c934355f8e48362
lxvs <jn.apsd@gmail.com>
2021-11-26 15:24:36 +0800

    232: Implement Queue Using Stacks

---
+++ 232-implement-queue-using-stacks.c
@@ -0,0 +1,68 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define STACK_MAX_SIZE      100
+
+typedef struct Stack {
+    int * val;
+    int top;
+} Stack;
+
+typedef struct {
+    Stack * bag;
+    Stack * stash;
+} MyQueue;
+
+MyQueue* myQueueCreate() {
+    MyQueue * p = malloc(sizeof *p);
+    p->bag = malloc(sizeof *p->bag);
+    p->bag->top = 0;
+    p->bag->val = malloc(sizeof *p->bag->val * STACK_MAX_SIZE);
+    p->stash = malloc(sizeof *p->stash);
+    p->stash->top = 0;
+    p->stash->val = malloc(sizeof *p->stash->val * STACK_MAX_SIZE);
+    return p;
+}
+
+static void stash (MyQueue * obj) {
+    while (obj->bag->top)
+        obj->stash->val[obj->stash->top++] = obj->bag->val[--obj->bag->top];
+}
+
+static void fetch (MyQueue * obj) {
+    while (obj->stash->top)
+        obj->bag->val[obj->bag->top++] = obj->stash->val[--obj->stash->top];
+}
+
+void myQueuePush(MyQueue* obj, int x) {
+    if (obj->bag->top == STACK_MAX_SIZE)
+        return;
+
+    stash(obj);
+    obj->bag->val[obj->bag->top++] = x;
+    fetch(obj);
+
+    return;
+}
+
+int myQueuePop(MyQueue* obj) {
+    return obj->bag->val[--obj->bag->top];
+}
+
+int myQueuePeek(MyQueue* obj) {
+    return obj->bag->val[obj->bag->top - 1];
+}
+
+bool myQueueEmpty(MyQueue* obj) {
+    return !obj->bag->top;
+}
+
+void myQueueFree(MyQueue* obj) {
+    free(obj->bag->val);
+    free(obj->bag);
+    free(obj->stash->val);
+    free(obj->stash);
+    free(obj);
+
+    return;
+}
