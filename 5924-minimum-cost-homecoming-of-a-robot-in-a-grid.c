88d923473a85a74737aace706d6a2026f4add3e1
lxvs <jn.apsd@gmail.com>
2021-11-28 01:09:51 +0800

    5924: Minimum Cost Homecoming of a Robot in a Grid - Manhattan distance

--- 5924-minimum-cost-homecoming-of-a-robot-in-a-grid.c
+++ 5924-minimum-cost-homecoming-of-a-robot-in-a-grid.c
@@ -1,49 +1,21 @@
 #include <stdlib.h>
 
 int minCost(int* startPos, int startPosSize, int* homePos, int homePosSize, int* rowCosts, int rowCostsSize, int* colCosts, int colCostsSize){
-    int ** costs = malloc(sizeof *costs * rowCostsSize);
-    int x = startPos[0];
-    int y = startPos[1];
-    int c1, c2;
+    int ret = 0;
 
-    for (int i = 0; i < rowCostsSize; i++)
-        costs[i] = malloc(sizeof **costs * colCostsSize);
+    if (startPos[0] < homePos[0])
+        for (int i = startPos[0] + 1; i <= homePos[0]; i++)
+            ret += rowCosts[i];
+    else if (startPos[0] > homePos[0])
+        for (int i = startPos[0] - 1; i >= homePos[0]; i--)
+            ret += rowCosts[i];
 
-    costs[x][y] = 0;
+    if (startPos[1] < homePos[1])
+        for (int i = startPos[1] + 1; i <= homePos[1]; i++)
+            ret += colCosts[i];
+    else if (startPos[1] > homePos[1])
+        for (int i = startPos[1] - 1; i >= homePos[1]; i--)
+            ret += colCosts[i];
 
-    for (int i = x - 1; i >= 0; i--)
-        costs[i][y] = costs[i + 1][y] + rowCosts[i];
-    for (int i = x + 1; i < rowCostsSize; i++)
-        costs[i][y] = costs[i - 1][y] + rowCosts[i];
-    for (int j = y - 1; j >= 0; j--)
-        costs[x][j] = costs[x][j + 1] + colCosts[j];
-    for (int j = y + 1; j < colCostsSize; j++)
-        costs[x][j] = costs[x][j - 1] + colCosts[j];
-
-    for (int i = x - 1; i >= 0; i--) {
-        for (int j = y - 1; j >= 0; j--) {
-            c1 = costs[i + 1][j] + rowCosts[i];
-            c2 = costs[i][j + 1] + colCosts[j];
-            costs[i][j] = c1 > c2 ? c2 : c1;
-        }
-        for (int j = y + 1; j < colCostsSize; j++) {
-            c1 = costs[i + 1][j] + rowCosts[i];
-            c2 = costs[i][j - 1] + colCosts[j];
-            costs[i][j] = c1 > c2 ? c2 : c1;
-        }
-    }
-    for (int i = x + 1; i < rowCostsSize; i++) {
-        for (int j = y - 1; j >= 0; j--) {
-            c1 = costs[i - 1][j] + rowCosts[i];
-            c2 = costs[i][j + 1] + colCosts[j];
-            costs[i][j] = c1 > c2 ? c2 : c1;
-        }
-        for (int j = y + 1; j < colCostsSize; j++) {
-            c1 = costs[i - 1][j] + rowCosts[i];
-            c2 = costs[i][j - 1] + colCosts[j];
-            costs[i][j] = c1 > c2 ? c2 : c1;
-        }
-    }
-
-    return costs[homePos[0]][homePos[1]];
+    return ret;
 }
