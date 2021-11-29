29614dce1ee68726c021946c7d1c9e400e3adb9b
lxvs <jn.apsd@gmail.com>
2021-11-29 11:02:10 +0800

    145: Binary Tree Postorder Traversal - recursion

---
+++ 145-binary-tree-postorder-traversal.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lib/btree.h"
+
+static void traverse(struct TreeNode * root, int * array, int * returnSize) {
+    if (!root)
+        return;
+    traverse(root->left, array, returnSize);
+    traverse(root->right, array, returnSize);
+    array[(*returnSize)++] = root->val;
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int * postorderTraversal(struct TreeNode * root, int * returnSize) {
+    int * ret = malloc(sizeof *ret * 100);
+    *returnSize = 0;
+    traverse(root, ret, returnSize);
+    return ret;
+}
