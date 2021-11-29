abe6c439354635f566e7eb337da4a516675b5fb5
lxvs <jn.apsd@gmail.com>
2021-11-29 21:51:22 +0800

    Almost done but missed a case
    Missed a case that later times are ahead in meetings

--- 5941-find-all-people-with-secret.c
+++ 5941-find-all-people-with-secret.c
@@ -1,54 +1,40 @@
 #include <stdlib.h>
 
-static void swap (int ** a, int ** b) {
-    int * tmp = *a;
-    *a = *b;
-    *b = tmp;
-    return;
-}
-
-static int ** sort (int ** meetings, int meetingsSize) {
-    for (int i = 0, swapped = 0; i < meetingsSize - 1; i++, swapped = 0) {
-        for (int j = 0; j < meetingsSize - 1 - i; j++)
-            if (meetings[j][2] > meetings[j + 1][2]) {
-                swap(meetings + j, meetings + j + 1);
-                swapped = 1;
-            }
-        if (!swapped)
-            break;
-    }
-    return meetings;
-}
-
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int * findAllPeople (int n, int ** meetings, int meetingsSize, int * meetingsColSize, int firstPerson, int * returnSize) {
     int * ret = malloc(sizeof *ret * n);
-    char * sec = malloc(sizeof *sec * n);
+    int * time = malloc(sizeof *time * n);
 
     *returnSize = 2;
     ret[0] = 0;
     ret[1] = firstPerson;
 
     for (int i = 1; i < n; i++)
-        sec[i] = 0;
-    sec[0] = 1;
-    sec[firstPerson] = 1;
+        time[i] = -1;
+    time[0] = 0;
+    time[firstPerson] = 0;
 
-    meetings = sort(meetings, meetingsSize);
     for (int i = 0; i < meetingsSize; i++) {
-        if (!(sec[meetings[i][0]] | sec[meetings[i][1]]) || sec[meetings[i][0]] & sec[meetings[i][1]])
+        int t = meetings[i][2];
+        int * t0 = time + meetings[i][0];
+        int * t1 = time + meetings[i][1];
+        if (*t0 + *t1 == -2 || ((*t0 + 1) && (*t1 + 1)))
             continue;
-        if (sec[meetings[i][0]]) {
-            sec[meetings[i][1]] = 1;
-            ret[(*returnSize)++] = meetings[i][1];
-        } else {
-            sec[meetings[i][0]] = 1;
+        if (*t0 == -1) {
+            if (t < *t1)
+                continue;
+            *t0 = t;
             ret[(*returnSize)++] = meetings[i][0];
+        } else {
+            if (t < *t0)
+                continue;
+            *t1 = t;
+            ret[(*returnSize)++] = meetings[i][1];
         }
     }
 
-    free(sec);
+    free(time);
     return ret;
 }
