c92bbf0d7bd87cb0a8307e1bf0e3f8269c0bef14
lxvs <jn.apsd@gmail.com>
2021-11-30 10:51:05 +0800

    101: Symmetric Tree - recursive

---
+++ 101-symmetric-tree.c
@@ -0,0 +1,15 @@
+#include "lib/btree.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
+static bool symmetric (struct TreeNode * left, struct TreeNode * right) {
+    if (!left || !right)
+        return left == right;
+    if (left->val != right->val)
+        return false;
+    return symmetric(left->left, right->right) && symmetric(left->right, right->left);
+}
+
+bool isSymmetric (struct TreeNode * root) {
+    return !root || symmetric(root->left, root->right);
+}
