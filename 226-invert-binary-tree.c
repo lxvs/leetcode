e4f87afd46948f0909d0d0c1c0c90b4c6492904d
lxvs <jn.apsd@gmail.com>
2021-12-01 10:26:09 +0800

    226: Invert Binary Tree - iterative

---
+++ 226-invert-binary-tree.c
@@ -0,0 +1,27 @@
+#include "lib/btree.h"
+#include <stdlib.h>
+
+static void swap (struct TreeNode ** a, struct TreeNode ** b) {
+    struct TreeNode * tmp = *a;
+    *a = *b;
+    *b = tmp;
+    return;
+}
+
+struct TreeNode * invertTree (struct TreeNode * root) {
+    if (!root)
+        return NULL;
+    struct TreeNode ** stack = malloc(sizeof *stack * 100);
+    unsigned char top = 0;
+    stack[top++] = root;
+
+    while (top) {
+        struct TreeNode * p = stack[--top];
+        swap(&p->left, &p->right);
+        if (p->left)
+            stack[top++] = p->left;
+        if (p->right)
+            stack[top++] = p->right;
+    }
+    return root;
+}
