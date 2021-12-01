f931dcec65e44d0ee41b44378cef3756752cca75
lxvs <jn.apsd@gmail.com>
2021-12-01 15:44:11 +0800

    701: Insert into a Binary Tree

---
+++ 701-insert-into-a-binary-tree.c
@@ -0,0 +1,24 @@
+#include "lib/btree.h"
+#include <stdlib.h>
+
+struct TreeNode * insertIntoBST (struct TreeNode * root, int val) {
+    if (!root) {
+        root = malloc(sizeof *root);
+        root->val = val;
+        root->left = root->right = NULL;
+        return root;
+    }
+    struct TreeNode * p = root;
+    while (p && ((val < p->val && p->left) || (val > p->val && p->right)))
+        p = val < p->val ? p->left : p->right;
+    if (val < p->val) {
+        p->left = malloc(sizeof *p);
+        p->left->val = val;
+        p->left->left = p->left->right = NULL;
+    } else {
+        p->right = malloc(sizeof *p);
+        p->right->val = val;
+        p->right->left = p->right->right = NULL;
+    }
+    return root;
+}
