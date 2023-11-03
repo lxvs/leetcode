2bb08de7657bf3143e93e23f381ee1cdf1c72f4f
Liu, Zhao-hui <lzhh@duck.com>
2023-11-03 09:46:18 +0800

    Add a script to replace includes

---
+++ utils/replace-header.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -o nounset
+
+main () {
+    local source_path_wo_ext=${1-}
+    local header_wo_ext
+    test "$source_path_wo_ext" || return
+    case ${source_path_wo_ext##*.} in
+    c|h|'')
+        source_path_wo_ext=${source_path_wo_ext%.*}
+        ;;
+    esac
+    header_wo_ext=$(basename "$source_path_wo_ext")
+    sed -e "/^\s*#include\s*\"$header_wo_ext.h\"\s*\$/{" \
+        -e "r $source_path_wo_ext.h" \
+        -e "d}" \
+        "$source_path_wo_ext.c"
+}
+
+main "$@"
