46ecbea646fa82f39a1605b70e27bc4496b34d95
Liu, Zhao-hui <lzhh@duck.com>
2023-11-08 22:51:01 +0800

    [WRONG ANSWER] 069: sqrt(x) binary search

--- easy/069-sqrtx.c
+++ easy/069-sqrtx.c
@@ -1,7 +1,25 @@
 #include "069-sqrtx.h"
 
 int mySqrt (int x) {
-    int i;
-    for (i = 1; x != 0 && x / i >= i; i++);
-    return ((x != 0) && (x / i == i) && (x % i == 0)) ? i : i - 1;
+    int low = 2;
+    int high = x;
+    long mid = low + (high - low) / 2;
+
+    if (x <= 1) {
+        return x;
+    }
+
+    while (low < high - 1) {
+        if (mid * mid == x) {
+            return mid;
+        }
+        if (mid * mid < x) {
+            low = mid;
+        } else {
+            high = mid;
+        }
+        mid = low + (high - low) / 2;
+    }
+
+    return mid;
 }
