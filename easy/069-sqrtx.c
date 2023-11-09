5c001d8a8605c8befc8b5eb36f8bb242c5cffc40
Liu, Zhao-hui <lzhh@duck.com>
2023-11-09 09:45:49 +0800

    Fix binary search approach for 069: sqrt(x)

--- easy/069-sqrtx.c
+++ easy/069-sqrtx.c
@@ -3,23 +3,22 @@
 int mySqrt (int x) {
     int low = 2;
     int high = x;
-    long mid = low + (high - low) / 2;
 
     if (x <= 1) {
         return x;
     }
 
-    while (low < high - 1) {
+    while (low <= high) {
+        long mid = low + (high - low) / 2;
         if (mid * mid == x) {
             return mid;
         }
         if (mid * mid < x) {
-            low = mid;
+            low = mid + 1;
         } else {
-            high = mid;
+            high = mid - 1;
         }
-        mid = low + (high - low) / 2;
     }
 
-    return mid;
+    return high;
 }
