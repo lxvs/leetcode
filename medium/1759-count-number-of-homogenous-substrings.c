5256c5c334991b7b74a7a785dc1f36eae21ed851
Liu, Zhao-hui <lzhh@duck.com>
2023-11-09 11:06:12 +0800

    1759 sliding window approach

--- medium/1759-count-number-of-homogenous-substrings.c
+++ medium/1759-count-number-of-homogenous-substrings.c
@@ -1,37 +1,20 @@
+#include <string.h>
 #include "1759-count-number-of-homogenous-substrings.h"
 
 #define SUM_MODULO      1000000007
 
-static inline int getFullCount (int n) {
-    int returnValue = 0;
-    for (int i = 1; i <= n; i++) {
-        returnValue = (returnValue + i) % SUM_MODULO;
-    }
-    return returnValue;
-}
-
-static inline void addToSum (int * sum, int addend) {
-    *sum = (*sum + getFullCount (addend)) % SUM_MODULO;
-}
-
 int countHomogenous (char * s) {
-    char current = s[0];
-    int count = 0;
+    int i = 0;
+    int j = 0;
+    int length = strlen (s);
     int sum = 0;
 
-    while (*s != '\0') {
-        if (current == *s) {
-            count++;
-            s++;
-            continue;
+    while (j < length) {
+        while (j < length && s[i] == s[j]) {
+            sum = (sum + (j - i + 1)) % SUM_MODULO;
+            j++;
         }
-        addToSum (&sum, count);
-        current = *s;
-        count = 0;
-    }
-
-    if (count > 0) {
-        addToSum (&sum, count);
+        i = j;
     }
 
     return sum;
