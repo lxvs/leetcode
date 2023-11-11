12b12bf24f80d95ecec8802bd2141f1fd63f9a94
Liu, Zhao-hui <lzhh@duck.com>
2023-11-11 17:31:10 +0800

    070 dynamic programming table approach

--- easy/070-climbing-stairs.c
+++ easy/070-climbing-stairs.c
@@ -1,11 +1,22 @@
+#include <stdlib.h>
 #include "070-climbing-stairs.h"
 
 int climbStairs (int n) {
-    if (n == 1) {
-        return 1;
+    int * result;
+    int returnValue;
+
+    if (n <= 2) {
+        return n;
     }
-    if (n == 2) {
-        return 2;
+
+    result = malloc (sizeof *result * n);
+    result[0] = 1;
+    result[1] = 2;
+    for (int i = 2; i < n; i++) {
+        result[i] = result[i - 1] + result[i - 2];
     }
-    return climbStairs (n - 1) + climbStairs (n - 2);
+
+    returnValue = result[n - 1];
+    free (result);
+    return returnValue;
 }
