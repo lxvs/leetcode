6ab89caf523a85fb3a76d5af6c87c5e76eba94ab
Liu, Zhao-hui <lzhh@duck.com>
2023-12-20 20:48:16 +0800

    1913: Merge sort

--- easy/1913-maximum-product-difference-between-two-pairs.c
+++ easy/1913-maximum-product-difference-between-two-pairs.c
@@ -1,34 +1,51 @@
+#include <stdlib.h>
 #include "1913-maximum-product-difference-between-two-pairs.h"
 
-static inline void swap (int * a, int * b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+static void merge_lists (int * nums, int left, int mid, int right) {
+    int * temp = malloc (sizeof *temp * (right - left + 1));
+    int i = left;
+    int j = mid + 1;
+    int k = 0;
+
+    while (i <= mid && j <= right) {
+        if (nums[i] <= nums[j]) {
+            temp[k++] = nums[i++];
+        } else {
+            temp[k++] = nums[j++];
+        }
+    }
+
+    while (i <= mid) {
+        temp[k++] = nums[i++];
+    }
+
+    while (j <= right) {
+        temp[k++] = nums[j++];
+    }
+
+    for (int s = 0; s < right - left + 1; s++) {
+        nums[left + s] = temp[s];
+    }
+
+    free (temp);
 }
 
-static void quick_sort (int * nums, int left, int right) {
-    int pivotIndex = left;
-    int pivot;
-    char luck = 0;
+static void merge_sort (int * nums, int left, int right) {
+    int mid;
 
-    if (left >= right) {
+    if (right <= left) {
         return;
     }
 
-    pivot = nums[pivotIndex];
+    mid = left + (right - left) / 2;
 
-    for (int i = pivotIndex + 1; i < right + 1; i++) {
-        if (nums[i] < pivot || (nums[i] == pivot && (luck = !luck))) {
-            swap (&nums[i], &nums[++pivotIndex]);
-        }
-    }
-    swap (&nums[left], &nums[pivotIndex]);
+    merge_sort (nums, left, mid);
+    merge_sort (nums, mid + 1, right);
 
-    quick_sort (nums, left, pivotIndex - 1);
-    quick_sort (nums, pivotIndex + 1, right);
+    merge_lists (nums, left, mid, right);
 }
 
 int maxProductDifference (int * nums, int numsSize) {
-    quick_sort (nums, 0, numsSize - 1);
+    merge_sort (nums, 0, numsSize - 1);
     return nums[numsSize - 1] * nums[numsSize - 2] - nums[1] * nums[0];
 }
