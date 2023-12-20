512f9343435aec43615b76f7cc46cab1d75507f7
Liu, Zhao-hui <lzhh@duck.com>
2023-12-20 12:25:11 +0800

    1913: Insertion sort
    https://leetcode.com/problems/maximum-product-difference-between-two-pairs/submissions/1123911746

--- easy/1913-maximum-product-difference-between-two-pairs.c
+++ easy/1913-maximum-product-difference-between-two-pairs.c
@@ -1,29 +1,19 @@
 #include "1913-maximum-product-difference-between-two-pairs.h"
 
-static inline void swap (int * a, int * b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-static void bubble_sort (int * nums, int numsSize) {
-    while (1) {
-        char swapped = 0;
-        int sortedSize = 0;
-        for (int i = 1; i < numsSize - sortedSize; i++) {
-            if (nums[i - 1] > nums[i]) {
-                swap (&nums[i - 1], &nums[i]);
-                swapped = 1;
+static void insertion_sort (int * nums, int numsSize) {
+    for (int i = 1, j; i < numsSize; i++) {
+        int num = nums[i];
+        for (j = i - 1; j >= 0; j--) {
+            if (num >= nums[j]) {
+                break;
             }
+            nums[j + 1] = nums[j];
         }
-        sortedSize++;
-        if (!swapped) {
-            break;
-        }
+        nums[j + 1] = num;
     }
 }
 
 int maxProductDifference (int * nums, int numsSize) {
-    bubble_sort (nums, numsSize);
+    insertion_sort (nums, numsSize);
     return nums[numsSize - 1] * nums[numsSize - 2] - nums[1] * nums[0];
 }
