a516dca1351ee256c2070fbd16f1a299ccd76671
Liu, Zhao-hui <lzhh@duck.com>
2023-12-20 11:58:42 +0800

    1913: Bubble sort

--- easy/1913-maximum-product-difference-between-two-pairs.c
+++ easy/1913-maximum-product-difference-between-two-pairs.c
@@ -6,21 +6,24 @@ static inline void swap (int * a, int * b) {
     *b = temp;
 }
 
-static void selection_sort (int * nums, int numsSize) {
-    int min_index = 0;
-
-    for (int i = 0; i < numsSize; i++) {
-        for (int j = i; j < numsSize; j++) {
-            if (nums[j] < nums[min_index]) {
-                min_index = j;
+static void bubble_sort (int * nums, int numsSize) {
+    while (1) {
+        char swapped = 0;
+        int sortedSize = 0;
+        for (int i = 1; i < numsSize - sortedSize; i++) {
+            if (nums[i - 1] > nums[i]) {
+                swap (&nums[i - 1], &nums[i]);
+                swapped = 1;
             }
         }
-        swap (&nums[i], &nums[min_index]);
-        min_index = i + 1;
+        sortedSize++;
+        if (!swapped) {
+            break;
+        }
     }
 }
 
 int maxProductDifference (int * nums, int numsSize) {
-    selection_sort (nums, numsSize);
+    bubble_sort (nums, numsSize);
     return nums[numsSize - 1] * nums[numsSize - 2] - nums[1] * nums[0];
 }
